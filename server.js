dd824e3badfefb5ea200a81a8ad58030aa1fefc3
sherin <eliassherin@gmail.com>
2016-09-20 07:20:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,14 +13,13 @@ app.get('/article.one',function(req,res){
    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 
-app.get('/article.two',function(req,res){
-    res.send('Article two request send and will be served soon');
+app.get('/article.one',function(req,res){
+   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
 
-app.get('/article.three',function(req,res){
-    res.send('Article three request send and will be served soon');
+app.get('/article.one',function(req,res){
+   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
 });
-
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
