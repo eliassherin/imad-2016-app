1d7bb10321407c2415173227f9965c64543495a8
sherin <eliassherin@gmail.com>
2016-09-27 21:49:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,35 +21,42 @@
 
 //counter code
 var button=document.getElementById('counter');
-var counter=0;
+//var counter=0;
 button.onclick=function()
 {
     
     
-    //var request=new XMLHttpRequest();
+    var request=new XMLHttpRequest();
     
-   // request.onreadystatechange=function()
- // {
-    //if(request.readystate===XMLHttpRequest.DONE)
-   //{
-   //if(request.status===200)
- // {
-    //var counter=request.responseText;
-            counter=counter+1;
+   request.onreadystatechange=function()
+  {
+ if(request.readystate===XMLHttpRequest.DONE)
+  {
+  if(request.status===200)
+  {
+ var counter=request.responseText;
+ //           counter=counter+1;
               var span=document.getElementById('count');
                 span.innerHTML=counter.toString();
-  //}
-   //}
-  //};
+  }
+  }
+  };
     
-//request.open("GET","http://eliassherin.imad.hasura-app.io/counter",true);
-//request.send(null);
+request.open("GET","http://eliassherin.imad.hasura-app.io/counter",true);
+request.send(null);
 };
 
-//var nameInput=document.getElementById('name');
-//var name=nameInput.value;
-//var submit=document.getElementById('submit_btn');
-//submit.onclick=funtion()
-//{
-    
-//};
+var nameInput=document.getElementById('name');
+var name=nameInput.value;
+var submit=document.getElementById('submit_btn');
+submit.onclick=funtion()
+{
+  var names=["name1","name2","name3","name4"];  
+  var list='';
+  for(var i=0;i<names.length;i++)
+  {
+      list=='<li>'+names[i]+'</li>';
+  }
+  var ul=document.getElementById('namelist');
+  ul.innerHTML=list;
+}
