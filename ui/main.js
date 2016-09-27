e26d9f7a711df1610d34bc813fd78db693038701
sherin <eliassherin@gmail.com>
2016-09-27 07:51:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,19 +1,34 @@
-console.log('Loaded!');
+//console.log('Loaded!');
 
 //change the test of main-text div
-var element=document.getElementById('main-text');
-element.innerHTML="New Value";
+//var element=document.getElementById('main-text');
+//element.innerHTML="New Value";
 
 //move the image
-var img=document.getElementById('madi');
-var marginLeft=0;
-function moveRight()
-{
-    marginLeft=marginLeft+1;
-    img.style.marginLeft= marginLeft + "px" ;
-}
-img.onclick = function()
-{
-    var interval=setInterval(moveRight,50);
+//var img=document.getElementById('madi');
+//var marginLeft=0;
+//function moveRight()
+//{
+ //   marginLeft=marginLeft+1;
+  //  img.style.marginLeft= marginLeft + "px" ;
+//}
+//img.onclick = function()
+//{
+ //   var interval=setInterval(moveRight,50);
     
+//};
+
+
+//counter code
+var button=document.getElementById('counter');
+var counter=0;
+button.onClick=function()
+{
+    
+    
+    
+    
+    counter=counter+1;
+   var span=document.getElementById('count');
+   span.innerHTML=counter.toString();
 };
