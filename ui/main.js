3525ec2bf0d0a6f6ade9afcd2a7c945c34fd0f63
sherin <eliassherin@gmail.com>
2016-09-27 21:00:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -21,26 +21,26 @@
 
 //counter code
 var button=document.getElementById('counter');
-//var counter=0;
+var counter=0;
 button.onClick=function()
 {
     
     
-    var request=new XMLHttpRequest();
+   // var request=new XMLHttpRequest();
     
-    request.onreadystatechange=function()
-    {
-       if(request.readystate===XMLHttpRequest.DONE)
-       {
-          if(request.status===200)
-          {
-              var counter=request.responseText;
+  //  request.onreadystatechange=function()
+    //{
+     //  if(request.readystate===XMLHttpRequest.DONE)
+      // {
+        //  if(request.status===200)
+          //{
+            //  var counter=request.responseText;
               var span=document.getElementById('count');
                 span.innerHTML=counter.toString();
-          }
-       }
-    };
+          //}
+      // }
+    //};
     
-    request.open("GET","http://eliassherin.imad.hasura-app.io/counter",true);
-    request.send(null);
+    //request.open("GET","http://eliassherin.imad.hasura-app.io/counter",true);
+    //request.send(null);
 };
