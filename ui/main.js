dc4563afb075f28e5121ac370838998fee693e48
sherin <eliassherin@gmail.com>
2016-09-27 22:10:41 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,7 +30,7 @@ button.onclick=function()
     
    request.onreadystatechange=function()
   {
- if(request.readystate===XMLHttpRequest.DONE)
+ if(request.readyState===XMLHttpRequest.DONE)
   {
   if(request.status===200)
   {
