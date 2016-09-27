3af917410b302e4cee05bd1b6a4bdf259fe83940
sherin <eliassherin@gmail.com>
2016-09-27 07:09:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,7 +6,7 @@ element.innerHTML="New Value";
 
 //move the image
 var img=document.getElementById('img');
-img.onclick=function()
+img.onclick = function()
 {
-    img.style.marginleft= "100px";
+    img.style.marginLeft= "100px";
 };
