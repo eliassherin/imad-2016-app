999ce4b0c9f1a1661b6cfbdba43e3dc5753d4ef1
sherin <eliassherin@gmail.com>
2016-09-27 07:45:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,13 +89,21 @@ function createTemplate(data){
         </body>
         
         </html>
+        `
+        ;
     return htmlTemplate;
 }
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-
+var counter=0;
+app.get('/counter',function(req,res)
+{
+    counter=counter+1;
+    res.send(counter.toString());
+}
+);
 app.get('/:articleName',function(req,res){
     var articleName=req.params.articleName;
    res.send(createTemplate(articles[articleName]));
@@ -112,7 +120,6 @@ app.get('/ui/madi.png', function (req, res) {
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
-
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
