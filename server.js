159b492f4292e47e5d2195c24777a69ea3a5a49b
sherin <eliassherin@gmail.com>
2016-09-28 07:52:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -122,10 +122,11 @@ app.get('/ui/main.js', function (req, res) {
 });
 
 var names=[];
-app.get('/submit-name/:name', function (req, res) {
-    var name=req.params.name;
-    names.push(name);
-    res.send(JSON.stringify(names));
+app.get('/submit-name', function (req, res) {
+   res.send("Hi");
+   // var name=req.params.name;
+//    names.push(name);
+ //   res.send(JSON.stringify(names));
     
 });
 
