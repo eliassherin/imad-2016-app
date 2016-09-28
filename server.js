3085d86e9ea6e702252987072fabb3e440486a2f
sherin <eliassherin@gmail.com>
2016-09-28 07:58:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,6 +104,17 @@ app.get('/counter',function(req,res)
     res.send(counter.toString());
 }
 );
+var names=[];
+//app.get('/submit-name/:name', function (req, res) {
+   app.get('/submit-name', function (req, res) {
+    //var name=req.params.name;
+    var name=req.query.name;
+   names.push(name);
+  res.send(JSON.stringify(names));
+    
+});
+
+
 app.get('/:articleName',function(req,res){
     var articleName=req.params.articleName;
    res.send(createTemplate(articles[articleName]));
@@ -121,15 +132,6 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-var names=[];
-//app.get('/submit-name/:name', function (req, res) {
-   app.get('/submit-name', function (req, res) {
-    //var name=req.params.name;
-    var name=req.query.name;
-   names.push(name);
-  res.send(JSON.stringify(names));
-    
-});
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
 app.listen(8080, function () {
