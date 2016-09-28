49a7bdb8b52bc2762f2a9f1339043d0fc5207513
sherin <eliassherin@gmail.com>
2016-09-28 07:52:57 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -121,7 +121,7 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-var names=[];
+//var names=[];
 app.get('/submit-name', function (req, res) {
    res.send("Hi");
    // var name=req.params.name;
