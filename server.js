5c659e19a2b83a416ababe22b236e6f6d43b6895
sherin <eliassherin@gmail.com>
2016-09-28 08:00:21 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -104,6 +104,7 @@ app.get('/counter',function(req,res)
     res.send(counter.toString());
 }
 );
+
 var names=[];
 //app.get('/submit-name/:name', function (req, res) {
    app.get('/submit-name', function (req, res) {
