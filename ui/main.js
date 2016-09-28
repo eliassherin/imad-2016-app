9b0c21583cbac85f57dc97f780aa9a5a6e2ff471
sherin <eliassherin@gmail.com>
2016-09-28 08:06:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -30,7 +30,17 @@ var nameInput=document.getElementById('name');
 var name=nameInput.value;
 var submit=document.getElementById('submit_btn');
 submit.onclick=function(){
-    var names=['name1','name2','name2','name4'];
+    
+      
+        var request=new XMLHttpRequest();
+        request.onreadystatechange =function(){
+          
+          if(request.readyState === XMLHttpRequest.DONE)
+          {
+              if(request.status === 200)
+              {
+                  var names=request.responseText;
+                  names=JSON.parse(names);
     var list='';
     for (var i=0;i<names.length;i++)
     {
@@ -38,5 +48,15 @@ submit.onclick=function(){
     }
     var ul=document.getElementById('namelist');
     ul.innerHTML=list;
+              }
+          }
+            
+        };
+       
+   
+       request.open('GET','http://eliassherin.imad.hasura-app.io/submit-name?name=' +name ,true);
+       request.send(null);
+       
+    
     
 };
